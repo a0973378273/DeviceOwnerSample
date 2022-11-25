3dd6d2bc022c4b6bc636a4bc06affefd09348614
DESKTOP-S2HLGBV\bean <bean>
2022-11-25 18:11:00 +0800

    Add requestDeviceAdmin function

--- app/src/main/java/bean/sample/deviceownersample/MainActivity.kt
+++ app/src/main/java/bean/sample/deviceownersample/MainActivity.kt
@@ -1,5 +1,8 @@
 package bean.sample.deviceownersample
 
+import android.app.admin.DevicePolicyManager
+import android.content.ComponentName
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +10,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        requestDeviceAdmin()
+    }
+
+    private fun requestDeviceAdmin(){
+        val deviceAdminComponent = ComponentName(this, DeviceAdminSample::class.java)
+        Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN).apply {
+            putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, deviceAdminComponent)
+            putExtra(DevicePolicyManager.EXTRA_ADD_EXPLANATION, "DEVICE_ADMIN_ADD_EXPLANATION")
+        }.run {
+            startActivity(this)
+        }
     }
 }
